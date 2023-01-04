ac8eeaaed27f76dfedc6e01d4610733174012bd6
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2023-01-04 16:39:53 -0300

    Update forms alert

--- src/components/forms/forms.jsx
+++ src/components/forms/forms.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import './forms.css';
 
 export default function Forms(){
+    function submitForm(){
+        const inputEmail = document.querySelector("input#email").value;
+        const inputTopic = document.getElementById("topic").value;
+        const inputMessage = document.querySelector("textarea").value;
+
+        if(inputEmail.length > 2 && inputTopic.length >= 2 && inputMessage.length >= 2){
+            alert("Mensagem enviada com sucesso");
+        } else{
+            alert("Todos os campos são obrigatórios, tente novamente.");
+        }
+    }
+
     return(
         <section class="containerForms">
             <span class="boxFormsAndTitle">
@@ -10,7 +22,7 @@ export default function Forms(){
                     <input id="email" name="email" type="email" placeholder="Diga o seu melhor email" required/>
                     <input id="topic" name="topic" type="text" placeholder="Assunto" required/>
                     <textarea id="textarea" name="textarea" placeholder="Escreva a sua mensagem" rows="5" cols="49" required/>
-                    <button class="btnForms">ENVIAR</button>
+                    <button class="btnForms" onClick={submitForm}>ENVIAR</button>
                 </forms>
             </span>
         </section>
